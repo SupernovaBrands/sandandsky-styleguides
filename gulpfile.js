9aeb49d8ea8d28de12357643d5c2db0166918ecc
Lusia Kristiana Purnama <lusiakpurnama@gmail.com>
2021-04-28 12:57:00 +0700

    add prepush hook

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const handlebars = require('gulp-compile-handlebars');
 const rename = require('gulp-rename');
+const fs = require('fs');
+const path = require('path');
 
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
@@ -15,9 +17,9 @@ const webpackConfig = require('./webpack.config.js');
 const cssDir = 'dist/css';
 
 const files = {
-	index: ['index.html'],
-	hbs: ['src/hbs/**/*.hbs', '!src/hbs/partials/*.hbs'],
-	partials: ['src/hbs/partials/*.hbs'],
+	index: ['src/docs/index.hbs'],
+	hbs: ['src/docs/**/*.hbs', '!src/docs/index.hbs'],
+	partials: ['src/partials/**/*.hbs'],
 	js: ['dist/js/**/*'],
 	allScss: ['src/scss/**/*'],
 	scss: ['src/scss/*.scss'],
@@ -28,11 +30,58 @@ function errorHandler(err) {
 	this.emit('end');
 }
 
-const indexFile = () => src(files.index)
-	.pipe(browserSync.stream());
+const titleCase = (str) => {
+	const split = str.toLowerCase().split('-');
+	for (let i = 0; i < split.length; i += 1) {
+		split[i] = split[i].charAt(0).toUpperCase() + split[i].slice(1);
+	}
+	return split.join(' ');
+};
+
+const hbsHelpers = {
+	titleCase,
+	eq: (a, b) => a === b,
+	gt: (a, b) => a > b,
+	times: (n, block) => {
+		let accum = '';
+		for (let i = 1; i <= n; i += 1) {
+			block.data.index = i; // eslint-disable-line no-param-reassign
+			block.data.first = i === 1; // eslint-disable-line no-param-reassign
+			block.data.last = i === n; // eslint-disable-line no-param-reassign
+			accum += block.fn(i);
+		}
+		return accum;
+	},
+};
+
+const indexFile = () => {
+	const base = 'src/docs';
+	const folders = ['core', 'components', 'compounds', 'sections', 'templates'];
+	const filenames = {};
+	folders.forEach((f) => {
+		const names = fs.readdirSync(`${base}/${f}`)
+			.filter((file) => path.extname(file) === '.hbs')
+			.map((file) => {
+				const name = file.replace('.hbs', '');
+				return { name: titleCase(name), path: `${f}/${name}.html` };
+			});
+		filenames[f] = names;
+	});
+	const core = 'Core & Components';
+	folders.splice(0, 2, core);
+	const result = {
+		folders,
+		filenames: { [core]: [...filenames.core, ...filenames.components], ...filenames },
+	};
+	return src(files.index)
+		.pipe(handlebars(result, { helpers: hbsHelpers }))
+		.pipe(rename({ extname: '.html' }))
+		.pipe(dest('.'))
+		.pipe(browserSync.stream());
+};
 
 const hbsFiles = () => src(files.hbs)
-	.pipe(handlebars({}, { batch: ['src/hbs/partials'] }))
+	.pipe(handlebars({}, { batch: ['src/partials'], helpers: hbsHelpers }))
 	.pipe(rename({ extname: '.html' }))
 	.pipe(dest('docs'))
 	.pipe(browserSync.stream());
@@ -63,7 +112,9 @@ const webpackBuild = (isWatch = false) => () => new Promise((resolve, reject) =>
 const watchFiles = (done) => {
 	watch(files.allScss, parallel(scssFiles));
 	watch(files.index, parallel(indexFile));
-	watch(files.hbs, parallel(hbsFiles));
+	watch(files.hbs, parallel(hbsFiles))
+		.on('add', indexFile)
+		.on('unlink', indexFile);
 	watch(files.partials, parallel(hbsFiles));
 	watch(files.js, parallel(jsFiles));
 	done();
@@ -89,16 +140,17 @@ const initServer = (done) => {
 const clean = () => del([cssDir]);
 
 task(clean);
+task(indexFile);
 task(scssFiles);
 task(initServer);
 task('webpack', webpackBuild());
 task('webpackWatch', webpackBuild(true));
 task(
 	'build',
-	parallel(hbsFiles, scssFiles, 'webpack'),
+	parallel(indexFile, hbsFiles, scssFiles, 'webpack'),
 );
 task('watch', series(
-	parallel(hbsFiles, scssFiles, 'webpackWatch'),
+	parallel(indexFile, hbsFiles, scssFiles, 'webpackWatch'),
 	watchFiles,
 ));
 task('default', series('clean', 'watch', 'initServer'));

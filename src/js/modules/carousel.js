db08747fb4428593bbde88b216d49026b555db7c
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2021-05-11 19:32:46 +0700

    add carousel-scroll, adjust spacings

--- src/js/modules/carousel.js
+++ src/js/modules/carousel.js
@@ -1,41 +1,80 @@
-import { screenLG } from '~mod/constants';
+if ($('.carousel--scroll').length > 0) {
+	const scrollbars = [];
 
-if ($('.carousel--loop').length > 0) {
-	// moving element carousel item depending of items per slide
-	// triggered by bootstrap carousel slide event (when transition started)
-	$('.carousel--loop').on('slide.bs.carousel', function (e) {
-		const $e = $(e.relatedTarget);
-		let idx = $e.index();
-		let itemsPerSlide = $(this).data('slide-number') ? $(this).data('slide-number') : 3;
-		const totalItems = $(this).find('.carousel-item').length;
+	const adjustScrollThumb = (thumb, inner) => {
+		// eslint-disable-next-line no-param-reassign
+		thumb.style.width = `${(inner.clientWidth / inner.scrollWidth) * 100}%`;
+		// eslint-disable-next-line no-param-reassign
+		thumb.style.left = `${(inner.scrollLeft / inner.scrollWidth) * 100}%`;
+	};
 
-		if (screenLG > window.innerWidth) {
-			// set 1 for mobile
-			itemsPerSlide = 2;
-		}
-
-		if ($(this).find('.carousel--centered').length > 0) {
-			// add 1 element for negative offset of carousel inner
-			idx += 1;
-
-			// special case for carousel centered we would need plus 1, as we have negative offset x on carousle-inner
-			if (e.direction === 'right') {
-				$(this).find(`.carousel-item:nth-child(${$(this).find('.carousel-item.active').index() + 1 + itemsPerSlide})`).addClass('carousel-item--last');
-			}
-		}
-		if (idx >= totalItems - (itemsPerSlide - 1)) {
-			const it = itemsPerSlide - (totalItems - idx);
-			for (let i = 0; i < it; i += 1) {
-				if (e.direction === 'left') {
-					$(this).find('.carousel-item').eq(i).appendTo($(this).find('.carousel-inner'));
-				} else {
-					$(this).find('.carousel-item').eq(0).appendTo($(this).find('.carousel-inner'));
-				}
-			}
-		}
+	window.addEventListener('resize', () => {
+		scrollbars.forEach((s) => adjustScrollThumb(s.thumb, s.inner));
 	});
 
-	$('.carousel--loop').on('slid.bs.carousel', function () {
-		$(this).find('.carousel-item--last').removeClass('carousel-item--last');
+	$('.carousel--scroll').each((index, carousel) => {
+		const inner = carousel.querySelector('.carousel-inner');
+		const item = carousel.querySelector('.carousel-item');
+		const scrollbar = carousel.querySelector('.scrollbar');
+		const scrollThumb = carousel.querySelector('.scrollbar--thumb');
+		const prevButton = carousel.querySelector('.carousel-control-prev');
+		const nextButton = carousel.querySelector('.carousel-control-next');
+
+		adjustScrollThumb(scrollThumb, inner);
+		scrollbars.push({ thumb: scrollThumb, inner });
+
+		let x = 0;
+		let left = 0;
+		let itemIndex = 0;
+
+		const innerDrag = (e) => {
+			inner.scrollLeft = left - (e.pageX || e.touches[0].pageX) + x;
+			scrollThumb.style.left = `${(inner.scrollLeft / inner.scrollWidth) * 100}%`;
+		};
+
+		const scrollDrag = (e) => {
+			inner.scrollLeft = left + ((e.pageX || e.touches[0].pageX) - x) * (inner.scrollWidth / scrollbar.clientWidth);
+			scrollThumb.style.left = `${(inner.scrollLeft / inner.scrollWidth) * 100}%`;
+		};
+
+		const eventStart = (e) => {
+			e.preventDefault();
+			x = (e.pageX || e.touches[0].pageX);
+			left = inner.scrollLeft;
+
+			document.addEventListener(
+				e.type === 'mousedown' ? 'mousemove' : 'touchmove',
+				e.target === scrollThumb ? scrollDrag : innerDrag,
+			);
+		};
+
+		inner.addEventListener('mousedown', eventStart, true);
+		inner.addEventListener('touchstart', eventStart, true);
+
+		scrollThumb.addEventListener('mousedown', eventStart, true);
+		scrollThumb.addEventListener('touchstart', eventStart, true);
+
+		document.addEventListener('mouseup', () => {
+			document.removeEventListener('mousemove', innerDrag);
+			document.removeEventListener('mousemove', scrollDrag);
+		});
+
+		document.addEventListener('touchend', () => {
+			document.removeEventListener('touchmove', innerDrag);
+			document.removeEventListener('touchmove', scrollDrag);
+		});
+
+		const scrollItem = (direction) => (e) => {
+			e.preventDefault();
+			itemIndex = Math.round(inner.scrollLeft / item.clientWidth) + (direction === 'left' ? -2 : 2);
+			left = itemIndex * item.clientWidth;
+			if (left < 0) left = 0;
+			else if (left > inner.scrollWidth - inner.clientWidth) left = inner.scrollWidth - inner.clientWidth;
+			$(inner).animate({ scrollLeft: left }, 300);
+			$(scrollThumb).animate({ left: `${(left / inner.scrollWidth) * 100}%` }, 300);
+		};
+
+		prevButton.addEventListener('mousedown', scrollItem('left'));
+		nextButton.addEventListener('mousedown', scrollItem('right'));
 	});
 }

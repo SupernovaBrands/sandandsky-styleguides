83e60aff181769c07ba99a989f0800a877fb4b05
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2021-05-12 10:17:18 +0700

    adjust a-beauty carousel in mobile

--- src/js/modules/carousel.js
+++ src/js/modules/carousel.js
@@ -78,3 +78,22 @@ if ($('.carousel--scroll').length > 0) {
 		nextButton.addEventListener('mousedown', scrollItem('right'));
 	});
 }
+
+if ($('#a-beauty-review').length > 0) {
+	let beforeIndicator;
+	let before2Indicator;
+	let afterIndicator;
+	let after2Indicator;
+	$('#a-beauty-review').on('slide.bs.carousel', function(e) {
+		beforeIndicator = e.to > 0 ? $(this).find('.carousel-indicators li').get(e.to - 1) : null;
+		before2Indicator = beforeIndicator ? $(beforeIndicator).prevAll() : null;
+		afterIndicator = e.to + 1 < $(this).find('.carousel-item').length ? $(this).find('.carousel-indicators li').get(e.to + 1) : null;
+		after2Indicator = afterIndicator ? $(afterIndicator).nextAll() : null;
+
+		$(this).find('.carousel-indicators li').removeClass('carousel-indicator--prev carousel-indicator--prev-out carousel-indicator--next carousel-indicator--next-out');
+		if (beforeIndicator) $(beforeIndicator).addClass('carousel-indicator--prev');
+		if (before2Indicator) $(before2Indicator).addClass('carousel-indicator--prev-out');
+		if (afterIndicator) $(afterIndicator).addClass('carousel-indicator--next');
+		if (after2Indicator) $(after2Indicator).addClass('carousel-indicator--next-out');
+	});
+}

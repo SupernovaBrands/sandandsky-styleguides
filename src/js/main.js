7425059d13edc072ab63a0733009ed4fb576caa2
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2021-06-15 15:04:08 +0700

    home featured products

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 if ($('.product-card').length > 0) {
 	import(/* webpackChunkName: 'product-card' */ '~mod/product-card');
 }
+
+if ($('.carousel--scroll').length > 0) {
+	import(/* webpackChunkName: 'carousel-scroll' */ '~mod/carousel-scroll');
+}

160a769d79c8b24a8da059908247fb6c45f2ed2b
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2021-06-16 10:35:59 +0700

    update js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
-if ($('.carousel').length > 0) {
-	import(/* webpackChunkName: 'carousel' */ '~mod/carousel');
+if ($('.hero-carousel').length > 0) {
+	import(/* webpackChunkName: 'hero-carousel' */ '~mod/hero-carousel');
 }

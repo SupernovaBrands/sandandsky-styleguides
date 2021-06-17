cc46d066fb87e1e0c64a62128a3999acbb97b5a3
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2021-06-17 13:49:19 +0700

    fix carousel animation direction

--- src/js/modules/a-beauty-review.js
+++ src/js/modules/a-beauty-review.js
@@ -3,6 +3,10 @@ let before2Indicator;
 let afterIndicator;
 let after2Indicator;
 $('.a-beauty-review').on('slide.bs.carousel', function (e) {
+	if ((e.from === 3 && e.to === 0) || (e.to === 3 && e.from === 0)) {
+		$(this).addClass('reverse-direction');
+	}
+
 	beforeIndicator = e.to > 0 ? $(this).find('.carousel-indicators li').get(e.to - 1) : null;
 	before2Indicator = beforeIndicator ? $(beforeIndicator).prevAll() : null;
 	afterIndicator = e.to + 1 < $(this).find('.carousel-item').length ? $(this).find('.carousel-indicators li').get(e.to + 1) : null;
@@ -13,4 +17,8 @@ $('.a-beauty-review').on('slide.bs.carousel', function (e) {
 	if (before2Indicator) $(before2Indicator).addClass('carousel-indicator--prev-out');
 	if (afterIndicator) $(afterIndicator).addClass('carousel-indicator--next');
 	if (after2Indicator) $(after2Indicator).addClass('carousel-indicator--next-out');
+});
+
+$('.a-beauty-review').on('slid.bs.carousel', function () {
+	$(this).removeClass('reverse-direction');
 });

62436153cd9fefc836ef8d4046c30715abd12429
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2021-06-17 03:45:22 +0000

    Deploying gh-pages from master 30f9276c618ad67c27718dcbbddffdb3d564268a

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
   \************************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("if ($('.product-card').length > 0) {\n  __webpack_require__.e(/*! import() | product-card */ \"product-card\").then(__webpack_require__.bind(__webpack_require__, /*! ./modules/product-card */ \"./src/js/modules/product-card.js\"));\n}\n\n//# sourceURL=webpack://sandandsky-styleguides/./src/js/main.js?");
+eval("if ($('.hero-carousel').length > 0) {\n  __webpack_require__.e(/*! import() | hero-carousel */ \"hero-carousel\").then(__webpack_require__.t.bind(__webpack_require__, /*! ./modules/hero-carousel */ \"./src/js/modules/hero-carousel.js\", 23));\n}\n\nif ($('.product-card').length > 0) {\n  __webpack_require__.e(/*! import() | product-card */ \"product-card\").then(__webpack_require__.bind(__webpack_require__, /*! ./modules/product-card */ \"./src/js/modules/product-card.js\"));\n}\n\nif ($('.carousel--scroll').length > 0) {\n  __webpack_require__.e(/*! import() | carousel-scroll */ \"carousel-scroll\").then(__webpack_require__.t.bind(__webpack_require__, /*! ./modules/carousel-scroll */ \"./src/js/modules/carousel-scroll.js\", 23));\n}\n\nif ($('.instagram-carousel').length > 0) {\n  __webpack_require__.e(/*! import() | instagram */ \"instagram\").then(__webpack_require__.bind(__webpack_require__, /*! ./modules/instagram */ \"./src/js/modules/instagram.js\"));\n}\n\n//# sourceURL=webpack://sandandsky-styleguides/./src/js/main.js?");
 
 /***/ })
 
@@ -49,6 +49,36 @@ eval("if ($('.product-card').length > 0) {\n  __webpack_require__.e(/*! import()
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/create fake namespace object */
+/******/ 	(() => {
+/******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
+/******/ 		var leafPrototypes;
+/******/ 		// create a fake namespace object
+/******/ 		// mode & 1: value is a module id, require it
+/******/ 		// mode & 2: merge all properties of value into the ns
+/******/ 		// mode & 4: return value when already ns object
+/******/ 		// mode & 16: return value when it's Promise-like
+/******/ 		// mode & 8|1: behave like require
+/******/ 		__webpack_require__.t = function(value, mode) {
+/******/ 			if(mode & 1) value = this(value);
+/******/ 			if(mode & 8) return value;
+/******/ 			if(typeof value === 'object' && value) {
+/******/ 				if((mode & 4) && value.__esModule) return value;
+/******/ 				if((mode & 16) && typeof value.then === 'function') return value;
+/******/ 			}
+/******/ 			var ns = Object.create(null);
+/******/ 			__webpack_require__.r(ns);
+/******/ 			var def = {};
+/******/ 			leafPrototypes = leafPrototypes || [null, getProto({}), getProto([]), getProto(getProto)];
+/******/ 			for(var current = mode & 2 && value; typeof current == 'object' && !~leafPrototypes.indexOf(current); current = getProto(current)) {
+/******/ 				Object.getOwnPropertyNames(current).forEach((key) => (def[key] = () => (value[key])));
+/******/ 			}
+/******/ 			def['default'] = () => (value);
+/******/ 			__webpack_require__.d(ns, def);
+/******/ 			return ns;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
@@ -79,7 +109,7 @@ eval("if ($('.product-card').length > 0) {\n  __webpack_require__.e(/*! import()
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return "chunk." + chunkId + ".js?h=" + "cdcee08c133cdbd72f3e" + "";
+/******/ 			return "chunk." + chunkId + ".js?h=" + {"hero-carousel":"6ade9096f343162ef10a","product-card":"cdcee08c133cdbd72f3e","carousel-scroll":"44b0d75e9422319ebba8","instagram":"3aec0301b262c6028dad"}[chunkId] + "";
 /******/ 		};
 /******/ 	})();
 /******/ 	

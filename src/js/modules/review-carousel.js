ada5db856b6964a2069e3b5081d12edca5db728a
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-06-19 11:23:56 +0700

    fix params

--- src/js/modules/review-carousel.js
+++ src/js/modules/review-carousel.js
@@ -1,8 +1,8 @@
-const toggleReview = (elem, mode) => {
+const toggleReview = (elem) => {
 	const figCaption = elem.closest('figcaption');
 	const moreBtn = figCaption.find('.review-card__more-text');
 
-	if (mode === 'expand') {
+	if ($(elem).hasClass('review-card__more')) {
 		moreBtn.removeClass('d-none');
 	} else {
 		moreBtn.addClass('d-none');
@@ -12,9 +12,9 @@ const toggleReview = (elem, mode) => {
 
 $('.review-card__more').on('click', function () {
 	$(this).addClass('d-none');
-	toggleReview($(this), 'expand');
+	toggleReview($(this));
 });
 
 $('.review-card__less').on('click', function () {
-	toggleReview($(this), 'hide');
+	toggleReview($(this));
 });

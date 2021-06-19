99c3fb4f1f3494fb2b06429af129eb35768b95e3
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-06-19 11:12:07 +0700

    scrollbar adjustment

--- src/js/modules/review-carousel.js
+++ src/js/modules/review-carousel.js
@@ -1,5 +1,4 @@
 $('.review-card__more').on('click', function () {
-	$(this).closest('.carousel-inner').addClass('review-carousel__visible');
 	$(this).addClass('d-none');
 	const figCaption = $(this).closest('figcaption');
 	figCaption.find('.review-card__more-text').removeClass('d-none');
@@ -10,5 +9,4 @@ $('.review-card__less').on('click', function () {
 	const figCaption = $(this).closest('figcaption');
 	figCaption.find('.review-card__more-text').addClass('d-none');
 	figCaption.find('.review-card__more').removeClass('d-none');
-	$(this).closest('.carousel-inner').addClass('review-carousel__visible');
 });

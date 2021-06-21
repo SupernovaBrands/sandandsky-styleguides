87c28ad85cb65084b9748f0d28fc371f3fa02ab0
Teddy Maulana <maulanateddy99@gmail.com>
2021-06-21 22:46:21 +0700

    header sections

--- src/js/main.js
+++ src/js/main.js
@@ -44,6 +44,77 @@ $(document).ready(function () {
 		toggleHTMLVideo($(this).find('video'), false);
 		toggleiFrameVideo($(this).find('iframe'), false);
 	});
+
+	// mobile menu toggle
+	function mobileMenuToggler() {
+		$('.mobile-nav').toggleClass('show');
+		$('body').toggleClass('offcanvas-active');
+	}
+
+	$('.navbar-toggler').on('click', function () {
+		mobileMenuToggler();
+	});
+
+	$('.mobile-nav').click(function (e) {
+		if (e.target !== e.currentTarget) return;
+		mobileMenuToggler();
+	});
+
+	// header navbar detect scroll top or down
+	let lastScrollTop;
+	let scrollTop = 0;
+	const navbarEl = $('.main-header');
+	const announceBar = $('.announcement-bar');
+	const navbarHeight = navbarEl.height();
+	const cookiesBanner = $('.cookies-banner');
+
+	const productSwatchMobile = $('.product-swatch-mobile');
+	const productSwatchTrigger = $('.product-swatch-mobile__trigger');
+
+	$(window).on('scroll', function () {
+		scrollTop = $(this).scrollTop();
+
+		if ($('.cookies-banner:not(.d-none)').length > 0) {
+			if (cookiesBanner.find('.collapse:not(.show)').length > 0) {
+				$('body').addClass('cookies-banner-show');
+			} else if (cookiesBanner.find('.collapse.show').length > 0) {
+				$('body').addClass('cookies-banner-show--expanded');
+			}
+			navbarEl.removeClass('position-fixed').removeClass('scrolled-up').removeClass('scrolled-down');
+			announceBar.removeClass('d-none');
+		} else {
+			navbarEl.addClass('position-fixed');
+			if (scrollTop < lastScrollTop) {
+				navbarEl.removeClass('scrolled-down').addClass('scrolled-up');
+				if (scrollTop <= 0) {
+					// remove scrolled up for mobile menu show properly
+					navbarEl.removeClass('position-fixed').removeClass('scrolled-up');
+					if (announceBar.length > 0) {
+						announceBar.removeClass('d-none');
+					}
+				}
+			} else if (scrollTop <= 0) {
+				// safari fix bounce effect
+				navbarEl.removeClass('position-fixed').removeClass('scrolled-up');
+			} else {
+				navbarEl.removeClass('scrolled-up').addClass('scrolled-down');
+				if (announceBar.length > 0 && scrollTop > navbarHeight) {
+					announceBar.addClass('d-none');
+				}
+			}
+		}
+
+		lastScrollTop = scrollTop;
+
+		if (window.innerWidth < screenLG && productSwatchTrigger.length > 0) {
+			const overSwatch = scrollTop > productSwatchTrigger.offset().top;
+			if (overSwatch && !productSwatchMobile.hasClass('show')) {
+				productSwatchMobile.addClass('show');
+			} else if (!overSwatch && productSwatchMobile.hasClass('show')) {
+				productSwatchMobile.removeClass('show');
+			}
+		}
+	});
 });
 
 if ($('.hero-carousel').length > 0) {

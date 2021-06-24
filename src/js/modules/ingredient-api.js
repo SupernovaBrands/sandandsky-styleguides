577c8e7165b763dce78b33f7d822c50376b14868
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-06-24 17:19:10 +0700

    ingredients modal api

--- src/js/modules/ingredient-api.js
+++ src/js/modules/ingredient-api.js
@@ -1,14 +1,21 @@
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjQzZGQ4MzdjMzAxMzM2OWM4ODJlYjQiLCJjbGllbnRuYW1lIjoic2FuZCBhbmQgc2t5IiwidXNlcm5hbWUiOiJzYW5kc2t5IiwicGFzc3dvcmQiOiIkMmEkMTAkWjNwRzNkUG8vUW16MjVLeTRFbUFUdVJDV1MzL0ZndHRUVHRwd2xxaFZmb1pMNmxBWThyL08iLCJlbWFpbCI6InJoaWFubmUuY2xpZmZvcmRAc3VwZXJub3ZhYnJhbmRzLmNvbSIsInBhY2thZ2UiOiJ1bHRhIGNvbnNjaW91cyBiZWF1dHkiLCJwZXJtaXNzaW9uIjoib3duZXIiLCJpYXQiOjE2MjQzODk4MjZ9.fxRhjKnsENhUOivqcDzzzzTZtBaeEy6l3Qp5j43d3Tg';
 
+const toCapitalize = (text) => {
+	const upperCase = text.toLowerCase().replace(/\b[a-z]/g, function (letter) {
+		return letter.toUpperCase();
+	});
+	return upperCase;
+};
+
 const listIngredients = (ingredientArray) => {
 	let serialize = '';
 	if (ingredientArray.length > 0) {
 		for (let i = 0; i <= ingredientArray.length - 1; i += 1) {
 			const ingName = ingredientArray[i].ingredientName;
-			const upperCase = ingName.toLowerCase().replace(/\b[a-z]/g, function (letter) {
-				return letter.toUpperCase();
-			});
-			serialize += `<a href="#" class="d-inline-block text-body text-capitalize mr-1" data-toggle="modal" data-target="#ingredientModal" data-name="${ingredientArray[i].ingredientName}">${upperCase},</a>`;
+			const note = ingredientArray[i].retailerNote;
+			const upperCase = toCapitalize(ingName);
+			const speciality = ingredientArray[i].specialty;
+			serialize += `<a href="#" class="d-inline-block text-body text-capitalize mr-1" data-toggle="modal" data-target="#ingredientModal" data-name="${ingName}" data-note="${note}" data-speciality="${speciality}">${upperCase},</a>`;
 		}
 	}
 	return serialize;
@@ -19,8 +26,8 @@ const parseIngredients = (responseData) => {
 	if (responseData.productIngredients.length > 0) {
 		$.each(responseData.productIngredients, function (k, obj) {
 			if (obj.functionGroup.length > 0) {
-				$.each(obj.functionGroup, function (k, obj) {
-					html += listIngredients(obj.ingredients);
+				$.each(obj.functionGroup, function (l, obj2) {
+					html += listIngredients(obj2.ingredients);
 				});
 			}
 		});
@@ -50,9 +57,18 @@ $('#collapseIngredients').on('show.bs.collapse', function () {
 });
 
 $('#ingredientModal').on('show.bs.modal', function (event) {
+
+	$('.tab-ingredient__modal-title').addClass('d-none');
+	$('.tab-ingredient__modal-definition').addClass('d-none');
+	$('.tab-ingredient__modal-note').addClass('d-none');
+	$('.tab-ingredient__modal-quality').addClass('d-none');
+	$('.tab-ingredient__modal-func').addClass('d-none');
+
 	const triggerBtn = $(event.relatedTarget).attr('data-name');
+	const dataNote = $(event.relatedTarget).attr('data-note');
 	const sku = $('#collapseIngredients').data('sku');
 	const cname = $('#collapseIngredients').data('cname');
+	const dataSp = $(event.relatedTarget).attr('data-speciality');
 	const params = {
 		async: true,
 		crossDomain: true,
@@ -65,6 +81,23 @@ $('#ingredientModal').on('show.bs.modal', function (event) {
 		processData: false,
 	};
 	$.ajax(params).done(function (response) {
-		console.log(response);
+		const cfmAttributes = response.cfmIngredientAttributes;
+		const cfmAttributesString = [];
+		if (cfmAttributes.length > 0) {
+			for (let i = 0; i <= cfmAttributes.length - 1; i += 1) {
+				cfmAttributesString[i] = cfmAttributes[i].attribute;
+			}
+		}
+		$('.tab-ingredient__modal-title').html(toCapitalize(triggerBtn)).removeClass('d-none');
+		$('.tab-ingredient__modal-definition').html(`<strong>Definition: </strong>${response.definition}`).removeClass('d-none');
+		if (dataNote !== '') {
+			$('.tab-ingredient__modal-note').html(`<strong>Note:</strong> ${dataNote}`).removeClass('d-none');
+		}
+		if (cfmAttributesString.length > 0) {
+			$('.tab-ingredient__modal-func').html(`<strong>Functions: </strong>${cfmAttributesString.join(', ')}.`).removeClass('d-none');
+		}
+		if (dataSp !== '') {
+			$('.tab-ingredient__modal-quality').html(`<strong>Qualities: </strong>${dataSp}`).removeClass('d-none');
+		}
 	});
 });

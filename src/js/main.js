9e5651e7b1215a5d034369ce4f54d75bc51a98cc
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2021-06-24 13:53:16 +0700

    ingredients api

--- src/js/main.js
+++ src/js/main.js
@@ -73,3 +73,7 @@ if ($('.a-beauty-review').length > 0) {
 if ($('.review-carousel').length > 0) {
 	import(/* webpackChunkName: 'review-carousel' */ '~mod/review-carousel');
 }
+
+if ($('.tab-ingredient').length > 0) {
+	import(/* webpackChunkName: 'ingredient-api' */ '~mod/ingredient-api');
+}

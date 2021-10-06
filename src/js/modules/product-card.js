832d337a2fa148039f317de5820f1d8eef100bd0
Teddy Maulana <teddy.maulana@supernovabrands.com>
2021-10-06 23:54:46 +0700

    fix lint warning

--- src/js/modules/product-card.js
+++ src/js/modules/product-card.js
@@ -18,14 +18,10 @@ if ($('.product-card__rating').length > 0) {
 
 if ($('.menu-product-rating').length > 0) {
 	$('.menu-product-rating').each((i, el) => {
-		console.log('p', $('.menu-product-rating').length)
 		const appKey = $(el).data('app-key');
 		const productId = $(el).data('product-id');
 		$.get(`https://api-cdn.yotpo.com/products/${appKey}/${productId}/bottomline`).done(function (data) {
 			const avg = Math.round(data.response.bottomline.average_score * 10) / 10;
-			
-			const totalReviewsText = data.response.bottomline.total_reviews > 1 ? 'Reviews' : 'Review';
-			console.log(data)
 			$(el).find('.menu-product-rating__review-text').html(`${avg}/5.0 (${data.response.bottomline.total_reviews})`);
 			$(el).prepend(`<div class="yotpo"><span class="d-flex text-secondary mr-1">${injectSvgClass(SvgStar)}</span></div>`);
 			$(el).removeClass('d-none').addClass('d-flex');

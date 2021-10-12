d487f56a17794e01de8f50bf4a309c586e2e569b
Teddy Maulana <teddy.maulana@supernovabrands.com>
2021-10-12 11:40:18 +0700

    new announcement bar with modal

---
+++ src/js/modules/announcement-bar-abtest.js
@@ -0,0 +1,22 @@
+$('.modal--newsletter').on('show.bs.modal', function (e) {
+	$('body').addClass('modal--newsletter-open');
+});
+
+$('.modal--newsletter').on('hidden.bs.modal', function (e) {
+	$('body').removeClass('modal--newsletter-open');
+	$('#newsletter-form').removeClass('d-none');
+	$('#account-form').addClass('d-none');
+	$('#completed-form').addClass('d-none');
+});
+
+$('#newsletter-form form').on('submit', function (e) {
+	e.preventDefault();
+	$('#newsletter-form').addClass('d-none');
+	$('#account-form').removeClass('d-none');
+})
+
+$('#account-form form').on('submit', function (e) {
+	e.preventDefault();
+	$('#account-form').addClass('d-none');
+	$('#completed-form').removeClass('d-none');
+})

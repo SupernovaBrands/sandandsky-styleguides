4673fdce51623b618f287065ae8e32ef0690da22
Teddy Maulana <teddy.maulana@supernovabrands.com>
2021-10-12 11:47:57 +0700

    clean lint errors

--- src/js/modules/announcement-bar-abtest.js
+++ src/js/modules/announcement-bar-abtest.js
@@ -1,8 +1,8 @@
-$('.modal--newsletter').on('show.bs.modal', function (e) {
+$('.modal--newsletter').on('show.bs.modal', function () {
 	$('body').addClass('modal--newsletter-open');
 });
 
-$('.modal--newsletter').on('hidden.bs.modal', function (e) {
+$('.modal--newsletter').on('hidden.bs.modal', function () {
 	$('body').removeClass('modal--newsletter-open');
 	$('#newsletter-form').removeClass('d-none');
 	$('#account-form').addClass('d-none');
@@ -13,10 +13,10 @@ $('#newsletter-form form').on('submit', function (e) {
 	e.preventDefault();
 	$('#newsletter-form').addClass('d-none');
 	$('#account-form').removeClass('d-none');
-})
+});
 
 $('#account-form form').on('submit', function (e) {
 	e.preventDefault();
 	$('#account-form').addClass('d-none');
 	$('#completed-form').removeClass('d-none');
-})
+});

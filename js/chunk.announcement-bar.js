a5aa3d60d1b979d3cc31db913e88691f4fb7d9e3
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2021-10-20 02:33:30 +0000

    Deploying gh-pages from master 8e66dd9106057c3571e4257faf3b510cabdff6a5

---
+++ js/chunk.announcement-bar.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunksandandsky_styleguides"] = self["webpackChunksandandsky_styleguides"] || []).push([["announcement-bar"],{
+
+/***/ "./src/js/modules/announcement-bar-abtest.js":
+/*!***************************************************!*\
+  !*** ./src/js/modules/announcement-bar-abtest.js ***!
+  \***************************************************/
+/***/ (() => {
+
+eval("$('.modal--newsletter').on('show.bs.modal', function () {\n  $('body').addClass('modal--newsletter-open');\n});\n$('.modal--newsletter').on('hidden.bs.modal', function () {\n  $('body').removeClass('modal--newsletter-open');\n  $('#newsletter-form').removeClass('d-none');\n  $('#account-form').addClass('d-none');\n  $('#completed-form').addClass('d-none');\n});\n$('#newsletter-form form').on('submit', function (e) {\n  e.preventDefault();\n  $('#newsletter-form').addClass('d-none');\n  $('#account-form').removeClass('d-none');\n});\n$('#account-form form').on('submit', function (e) {\n  e.preventDefault();\n  $('#account-form').addClass('d-none');\n  $('#completed-form').removeClass('d-none');\n});\n\n//# sourceURL=webpack://sandandsky-styleguides/./src/js/modules/announcement-bar-abtest.js?");
+
+/***/ })
+
+}]);

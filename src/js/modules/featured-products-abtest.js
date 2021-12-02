4667a2965501b1e2cf7775d5f336dd822f349fd5
Teddy Maulana <teddy.maulana@supernovabrands.com>
2021-12-02 16:22:28 +0700

    remove unused codes

--- src/js/modules/featured-products-abtest.js
+++ src/js/modules/featured-products-abtest.js
@@ -1,27 +1,3 @@
-const adjustScrollThumb = (thumb, inner, scrollParent) => {
-	let innerOuterWidth;
-	if ($(inner).closest('.instagram-carousel').length > 0) {
-		// instagram scroll bugfix: out of container
-		// round to 1 decimal of item width
-		const itemWidth = Math.round($(inner).find('.carousel-item').outerWidth() * 10) / 10;
-		// instagram total images from instagram.js
-		innerOuterWidth = itemWidth * 15;
-	} else {
-		innerOuterWidth = inner.scrollWidth;
-	}
-	// eslint-disable-next-line no-param-reassign
-	thumb.style.width = `${(inner.clientWidth / innerOuterWidth) * 100}%`;
-	// eslint-disable-next-line no-param-reassign
-	thumb.style.left = `${(inner.scrollLeft / inner.scrollWidth) * 100}%`;
-	if (inner.clientWidth === inner.scrollWidth) {
-		inner.classList.add('justify-content-center');
-		scrollParent.classList.add('d-none');
-	} else {
-		inner.classList.remove('justify-content-center');
-		scrollParent.classList.remove('d-none');
-	}
-};
-
 $('#tabFeaturedProductContentFeatured .carousel--scroll').each((index, carousel) => {
 	carousel.classList.remove('d-none');
 	carousel.dispatchEvent(new CustomEvent('adjustThumb'));

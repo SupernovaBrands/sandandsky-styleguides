4dcb6296febef359f0a439e27190e3471ed4f88f
rizkiaprilian <rizki.a.aprilian@gmail.com>
2021-12-16 14:55:19 +0700

    update carousel event

--- src/js/modules/product-template.js
+++ src/js/modules/product-template.js
@@ -71,7 +71,7 @@ if (productWaitlistForm.length > 0 && productWaitlistSubmitted.length > 0) {
 $('.product-form [name=product-variant]').on('change', function () {
 	const carouselIndicator = $('.product-image-carousel__indicator .product-image-carousel__indicator__item');
 	const imageVariant = $(this).data('image');
-	if (imageVariant && carouselIndicator.length) {
+	if (imageVariant && carouselIndicator.length > 0) {
 		const targetIndicator = carouselIndicator.find(`img[src='${imageVariant}']`).closest('.product-image-carousel__indicator__item').data('slide-to');
 		$('#product-image-carousel').carousel(targetIndicator);
 	}

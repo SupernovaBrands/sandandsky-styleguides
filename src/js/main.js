3ded3d9c00ae37c5b515462a2c0d0bb077bec393
rizkiaprilian <rizki.a.aprilian@gmail.com>
2021-12-21 13:11:34 +0700

    user login dropdown abtest

--- src/js/main.js
+++ src/js/main.js
@@ -113,3 +113,7 @@ if ($('.modal--sweepstakes').length > 0) {
 if ($('.announcement-bar__timer').length > 0) {
 	import(/* webpackChunkName: 'announcement-timer' */ '~mod/announcement-timer');
 }
+
+if ($('.dropdown-menu--ab-test').length > 0) {
+	import(/* webpackChunkName: 'dropdown-abtest' */ '~mod/dropdown-abtest');
+}

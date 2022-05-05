b0b6ee34d90ad247f403c7fd5c3ab29feeecf5c6
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2022-05-05 04:35:24 +0000

    Deploying gh-pages from master ec74e4f378b9d21ea07cf97837c40b5a3b4b7040

--- js/chunk.carousel-scroll.js
+++ js/chunk.carousel-scroll.js
@@ -28,13 +28,13 @@ eval("$('.featured-products-aea-abtest .carousel--scroll').each(function (index,
 
 /***/ }),
 
-/***/ "./src/js/modules/range-section-abtest.js":
-/*!************************************************!*\
-  !*** ./src/js/modules/range-section-abtest.js ***!
-  \************************************************/
+/***/ "./src/js/modules/featured-products-tab.js":
+/*!*************************************************!*\
+  !*** ./src/js/modules/featured-products-tab.js ***!
+  \*************************************************/
 /***/ (() => {
 
-eval("$('#rangeTabContent .carousel--scroll').each(function (index, carousel) {\n  carousel.classList.remove('d-none');\n  carousel.dispatchEvent(new CustomEvent('adjustThumb'));\n});\n$('#rangeTab a[data-toggle=\"tab\"]').on('shown.bs.tab', function () {\n  $('.carousel--scroll').each(function (index, carousel) {\n    carousel.classList.remove('d-none');\n    carousel.dispatchEvent(new CustomEvent('adjustThumb'));\n  });\n});\n\n//# sourceURL=webpack://sandandsky-styleguides/./src/js/modules/range-section-abtest.js?");
+eval("$('#tabFeaturedProductContentFeatured .carousel--scroll').each(function (index, carousel) {\n  carousel.classList.remove('d-none');\n  carousel.dispatchEvent(new CustomEvent('adjustThumb'));\n});\n$('#tabFeaturedProductHead a[data-toggle=\"tab\"]').on('shown.bs.tab', function () {\n  $('.carousel--scroll').each(function (index, carousel) {\n    carousel.classList.remove('d-none');\n    carousel.dispatchEvent(new CustomEvent('adjustThumb'));\n  });\n});\n\n//# sourceURL=webpack://sandandsky-styleguides/./src/js/modules/featured-products-tab.js?");
 
 /***/ })
 

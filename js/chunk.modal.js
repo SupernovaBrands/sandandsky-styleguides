516ab00bef1a4a7e2bf6f4fe6e66a66e7ad31835
Lusia Kristiana Purnama <lusia.purnama@supernovabrands.com>
2022-06-15 02:50:05 +0000

    Deploying gh-pages from master bbdf45c77adf6083bd8c500840689f74e78004bf

--- js/chunk.modal.js
+++ js/chunk.modal.js
@@ -16,6 +16,17 @@
 
 eval("$('.modal--newsletter').on('show.bs.modal', function () {\n  $('body').addClass('modal--newsletter-open');\n});\n$('.modal--newsletter').on('hidden.bs.modal', function () {\n  $('body').removeClass('modal--newsletter-open');\n  $('#newsletter-form').removeClass('d-none');\n  $('#account-form').addClass('d-none');\n  $('#completed-form').addClass('d-none');\n});\n$('#newsletter-form form').on('submit', function (e) {\n  e.preventDefault();\n  $('#newsletter-form').addClass('d-none');\n  $('#account-form').removeClass('d-none');\n});\n$('#account-form form').on('submit', function (e) {\n  e.preventDefault();\n  $('#account-form').addClass('d-none');\n  $('#completed-form').removeClass('d-none');\n});\n$('.countries-options__select').on('change', function () {\n  var val = $(this).val();\n  var maskingEl = $('.countries-options__label');\n  var phoneCode = $(this).find(\"option[value='\".concat(val, \"']\")).data('code');\n  maskingEl.text(\"+\".concat(phoneCode)).addClass('selected');\n  $(this).trigger('mouseleave');\n}); // $('.modal--sweepstakes-cta').modal('show');\n\n//# sourceURL=webpack://sandandsky-styleguides/./src/js/modules/modal-sweepstakes.js?");
 
+/***/ }),
+
+/***/ "./src/js/modules/product-waitlist.js":
+/*!********************************************!*\
+  !*** ./src/js/modules/product-waitlist.js ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ \"./src/js/modules/utils.js\");\n\n\nvar phoneSubmitted = function phoneSubmitted(el) {\n  $(el).find('.modal--waitlist__phone').addClass('d-none');\n  $(el).find('.modal--waitlist__phone-submitted').removeClass('d-none').addClass('d-flex');\n};\n\nvar emailSubmitted = function emailSubmitted(el) {\n  $(el).find('.modal--waitlist__email').addClass('d-none');\n  $(el).find('.modal--waitlist__email-submitted').removeClass('d-none').addClass('d-flex');\n};\n\nvar formListener = function formListener() {\n  $('.modal--waitlist form, .product__waitlist-form').on('submit', function (e) {\n    e.preventDefault();\n    var el = e.target;\n    var email = $(el).find('input[name=\"email\"]').val();\n    var phone = $(el).find('input[name=\"phone\"]').val() || '';\n    var phoneValid = phone !== '' && (0,_utils__WEBPACK_IMPORTED_MODULE_0__.validatePhone)(phone);\n    var emailValid = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.validateEmail)(email);\n\n    if (emailValid || phoneValid) {\n      if (emailValid) {\n        emailSubmitted(el);\n      }\n\n      if (phoneValid) {\n        phoneSubmitted(el);\n      }\n    }\n  });\n};\n\nformListener();\n$('.modal--waitlist').modal('show');\n\n//# sourceURL=webpack://sandandsky-styleguides/./src/js/modules/product-waitlist.js?");
+
 /***/ })
 
 }]);
